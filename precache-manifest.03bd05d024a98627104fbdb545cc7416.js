eac816870f5157f1e006bd6a969fe9c3a07ae9f4
Jana-Z <github3@jana-zeller.de>
2020-06-25 17:21:33 +0200

    Updates

---
+++ precache-manifest.03bd05d024a98627104fbdb545cc7416.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41556b010dc16919b74fb5ea43d7e07",
+    "url": "/who-said-that/index.html"
+  },
+  {
+    "revision": "3772cf5b34f19cedb9f6",
+    "url": "/who-said-that/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3772cf5b34f19cedb9f6",
+    "url": "/who-said-that/static/js/2.630bf406.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/who-said-that/static/js/2.630bf406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6af679e9c9c8e7293c8",
+    "url": "/who-said-that/static/js/main.5716b6a2.chunk.js"
+  },
+  {
+    "revision": "e5dd5b133b4db6833336",
+    "url": "/who-said-that/static/js/runtime-main.b46e509d.js"
+  },
+  {
+    "revision": "2737bcbbfc88eaa92c1f27b337193b53",
+    "url": "/who-said-that/static/media/gryffindor.2737bcbb.svg"
+  },
+  {
+    "revision": "56338380da74d156db3ea5da0280b80b",
+    "url": "/who-said-that/static/media/happy.56338380.svg"
+  },
+  {
+    "revision": "41465dbe8b69728e9fac2a71d9253360",
+    "url": "/who-said-that/static/media/hufflepuff.41465dbe.svg"
+  },
+  {
+    "revision": "a04fba1c6c96d83a3b39f7bfcb5c11f2",
+    "url": "/who-said-that/static/media/map2.a04fba1c.jpg"
+  },
+  {
+    "revision": "a8316eee32e429ef6a70f878cd0ee59b",
+    "url": "/who-said-that/static/media/ravenclaw.a8316eee.svg"
+  },
+  {
+    "revision": "a704d0d3ea9d2c1e208f8859e5240e87",
+    "url": "/who-said-that/static/media/sad.a704d0d3.svg"
+  },
+  {
+    "revision": "1d2ca64f05a8eedc9e286fe567ffefca",
+    "url": "/who-said-that/static/media/slytherin.1d2ca64f.svg"
+  }
+]);
